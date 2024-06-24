6e6a5899581961691cdd8ba0d993935bdf83837f
Muragaruhae <jac8for@gmail.com>
2024-06-24 23:47:57 +0900

    [add] Gem annotate の追加
    annotateを追加した。カラムの情報を可視化する

--- app/models/binary.rb
+++ app/models/binary.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: binaries
+#
+#  id         :integer          not null, primary key
+#  context    :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Binary < ApplicationRecord
 end
