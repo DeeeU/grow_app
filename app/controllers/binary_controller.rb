6a0e06d3f6b3de29027baefb0666e6e2f36df5c3
Muragaruhae <jac8for@gmail.com>
2024-07-14 17:47:47 +0900

    [add] BinaryのコントローラとViewの追加

---
+++ app/controllers/binary_controller.rb
@@ -0,0 +1,10 @@
+class BinaryController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'binary/index'
+  get 'binary/show'
+  get 'binary/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
