b9c92b3875f7178894c7ed39f2920a73cd797b6c
Muragaruhae <jac8for@gmail.com>
2024-07-14 19:05:50 +0900

    [change] ルートの変更
    showページなど各id毎に表示したいページに遷移できるようにresourcesで変更。
    
    また、仮置きで画面の遷移ができるようにlinkを追加

--- app/controllers/binary_controller.rb
+++ app/controllers/binary_controller.rb
@@ -1,10 +1,16 @@
 class BinaryController < ApplicationController
   def index
+    @binaries = Binary.all
   end
 
   def show
+    @binary = Binary.find(params[:id])
   end
 
   def create
+  end
+
+  def destroy
+    @binary = Binary.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'binary/index'
-  get 'binary/show'
-  get 'binary/create'
+  resources :binary
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
