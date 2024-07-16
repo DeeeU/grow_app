df06fdbba1f7045541d63167de8dd407c1600340
Muragaruhae <jac8for@gmail.com>
2024-07-16 23:29:03 +0900

    [add] 日記の作成をできるようにした
    新規作成ボタンと作成時のフォームと導線作りを行った。
    binaryを複数形にしないとと今思った

--- app/controllers/binary_controller.rb
+++ app/controllers/binary_controller.rb
@@ -7,10 +7,27 @@ class BinaryController < ApplicationController
     @binary = Binary.find(params[:id])
   end
 
+  def new
+    @binary = Binary.new
+  end
+
   def create
+    @binary = Binary.new(binary_params)
+    if @binary.save
+      flash.now[:success] = '日記の投稿完了'
+      redirect_to binary_index_path
+    else
+      render :new
+    end
   end
 
   def destroy
     @binary = Binary.find(params[:id])
+  end
+
+  private
+
+  def binary_params
+    params.require(:binary).permit(:title, :context)
   end
 end
