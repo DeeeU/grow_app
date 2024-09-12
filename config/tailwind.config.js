e3e00e4e0b03973679fd6ba1cf963eb3f6c1b441
Muragaruhae <jac8for@gmail.com>
2024-09-12 23:43:39 +0900

    [add] 簡単なデフォルトのdesign追加

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      backgroundColor: {
+        'default-bg': '#f7fafc', // デフォルトの背景色を指定
+      },
+      fontSize: {
+        'default-title': '2.25rem', // デフォルトのタイトルサイズを指定 (36px)
+      },
+      textColor: {
+        'default-title-color': '#1e40af', // デフォルトのタイトル文字色を指定
+      },
     },
   },
   plugins: [
