9edc0742a46721bc4dbd7db2c9e700c91bf8c227
Muragaruhae <jac8for@gmail.com>
2024-10-14 16:09:36 +0900

    [add] タイトル検索のテストを追加した

--- spec/models/binary_spec.rb
+++ spec/models/binary_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Binary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'scopes' do
+    describe '.search_title' do
+      context 'タイトルが検索された時' do
+        let!(:true_binary){create(:binary, title: 'true_binary')}
+        let!(:false_binary) { create(:binary, title: 'false_binary') }
+
+        it '検索された文字列を含むタイトルの日記が返ってくる' do
+          expect(Binary.search_title('true').count).to eq(1)
+          expect(Binary.search_title('true')).to include(true_binary)
+        end
+      end
+    end
+  end
 end
