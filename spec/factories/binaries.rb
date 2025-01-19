512519aa221fb07733a1eca016f3a4a8445d5fcc
Muragaruhae <jac8for@gmail.com>
2025-01-19 15:54:10 +0900

    sorceryの追加
    userモデルを追加したく、追加する際にある程度の認証認可を簡単に導入できる&willnetさんがテコを入れてくださっているgemであるsorceryでuserを導入

--- spec/factories/binaries.rb
+++ spec/factories/binaries.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: binaries
+#
+#  id         :integer          not null, primary key
+#  context    :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :binary do
     title { "Sample Title" }
