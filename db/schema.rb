23b387eb2e2769785bacd7a371b80ebd924f5996
Muragaruhae <jac8for@gmail.com>
2025-01-19 15:54:10 +0900

    sorceryの追加
    userモデルを追加したく、追加する際にある程度の認証認可を簡単に導入できる&willnetさんがテコを入れてくださっているgemであるsorceryでuserを導入

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_15_141628) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
