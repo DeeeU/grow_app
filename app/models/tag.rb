ac0475fabe5a02b610f1da40d602250ab12a30db
Muragaruhae <jac8for@gmail.com>
2025-04-20 12:41:13 +0900

    リファクタとインスタンス変数をview側で使わないようにする

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -11,4 +11,13 @@
 #
 class Tag < ApplicationRecord
   has_and_belongs_to_many :binaries
+
+  # def self.find_or_create_from_params(tag_params)
+  #   reutrn nil if tag_params.present? && tag_params[:name].present?
+
+  #   find_or_create_by(tag_params[:name]) do |t|
+  #     t.context = tag_params[:context]
+  #     t.color = tag_params[:color] || '#FFFFFF'
+  #   end
+  # end
 end
